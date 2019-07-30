90ad84bcf88a4b41f03ba3c4b179acbff1ad4a68
DevShinobu <50808176+DevShinobu@users.noreply.github.com>
2019-07-30 15:19:55 -0300

    Create guildMemberAdd.js

---
+++ eventos/guildMemberAdd.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+module.exports.run = (client, member) => { //client+ parametros do evento
+  // código do evento, eventos disponíveis em client#events em discord.js.org
+}
